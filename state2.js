eac99248fa5fb9b88719d1698f7ebe9296179b66
Vinicius Vasconcelos <www.vasconcelos@gmail.com>
2018-04-18 17:04:01 -0300

    Implementação state2, demo Canhão

--- state2.js
+++ state2.js
@@ -1,10 +1,39 @@
+var canhao, bolas, velocidade = 800;
 demo.state2 = function () {};
 demo.state2.prototype = {
-	preload: function(){},
+	preload: function(){
+		game.load.image('base', 'assets/sprites/base.png');
+		game.load.image('canhao', 'assets/sprites/canhao.png');
+		game.load.image('bola', 'assets/sprites/bola.png');
+	},
 	create: function(){
-		game.stage.backgroundColor = '#ff5500';
+		game.stage.backgroundColor = '#aadddd';
 		console.log('state2');
 		addChangeStateEventListeners();
+
+		var base = game.add.sprite(centerX+30, centerY, 'base');
+		base.anchor.setTo(0.5);
+
+		canhao = game.add.sprite(centerX, centerY, 'canhao');
+		canhao.anchor.setTo(0.4, 0.5);
+
+		bolas = game.add.group();
+		bolas.enableBody = true;
+		bolas.physicsBodyType = Phaser.Physics.ARCADE;
+		bolas.createMultiple(50, 'bola');
 	},
-	update: function(){}
+	update: function(){
+		canhao.rotation = game.physics.arcade.angleToPointer(canhao);
+		if (game.input.activePointer.isDown) {
+			this.fire();
+		}
+	},
+
+	fire: function() {
+		console.log('PEW');
+		var bola = bolas.getFirstDead();
+		bola.reset(canhao.x, canhao.y);
+
+		game.physics.arcade.moveToPointer(bola, velocidade);
+	}
 };
